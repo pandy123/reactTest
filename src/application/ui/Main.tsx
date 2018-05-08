552280f4e52457668dfd1d0496fa697445a3df12
huangyb <huangyb@skong.com>
2018-05-08 18:06:53 +0800

    header 基础

--- src/application/ui/Main.tsx
+++ src/application/ui/Main.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import * as  ReactDOM from "react-dom";
-import { SideMenu } from "./Menu"
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { SideMenu } from "./SideMenu"
+import { Layout, Breadcrumb } from 'antd';
+import { Avatar } from 'antd';
+import { Menu, Dropdown, Icon } from 'antd';
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -11,60 +13,33 @@ export class Main extends React.Component<any, any> {
       // 设置属性
    }
    public render() {
+      const menu = (
+         <Menu>
+            <Menu.Item>
+               <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">个人信息</a>
+            </Menu.Item>
+            <Menu.Item>
+               <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">退出</a>
+            </Menu.Item>
+         </Menu>
+      );
       return (
          <Layout>
-            <Header className="header">
-               <div className="logo" />
-               <Menu
-                  theme="dark"
-                  mode="horizontal"
-                  defaultSelectedKeys={['2']}
-                  style={{ lineHeight: '64px' }}
-               >
-                  <Menu.Item key="1">nav 1</Menu.Item>
-                  <Menu.Item key="2">nav 2</Menu.Item>
-                  <Menu.Item key="3">nav 3</Menu.Item>
-               </Menu>
-            </Header>
-            <Content style={{ padding: '0 50px' }}>
-               <Breadcrumb style={{ margin: '16px 0' }}>
-                  <Breadcrumb.Item>Home</Breadcrumb.Item>
-                  <Breadcrumb.Item>List</Breadcrumb.Item>
-                  <Breadcrumb.Item>App</Breadcrumb.Item>
-               </Breadcrumb>
-               <Layout style={{ padding: '24px 0', background: '#fff' }}>
-                  <Sider width={200} style={{ background: '#fff' }}>
-                     <Menu
-                        mode="inline"
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
-                        style={{ height: '100%' }}
-                     >
-                        <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-                           <Menu.Item key="1">option1</Menu.Item>
-                           <Menu.Item key="2">option2</Menu.Item>
-                           <Menu.Item key="3">option3</Menu.Item>
-                           <Menu.Item key="4">option4</Menu.Item>
-                        </SubMenu>
-                        <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-                           <Menu.Item key="5">option5</Menu.Item>
-                           <Menu.Item key="6">option6</Menu.Item>
-                           <Menu.Item key="7">option7</Menu.Item>
-                           <Menu.Item key="8">option8</Menu.Item>
-                        </SubMenu>
-                        <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-                           <Menu.Item key="9">option9</Menu.Item>
-                           <Menu.Item key="10">option10</Menu.Item>
-                           <Menu.Item key="11">option11</Menu.Item>
-                           <Menu.Item key="12">option12</Menu.Item>
-                        </SubMenu>
-                     </Menu>
-                  </Sider>
-                  <Content style={{ padding: '0 24px', minHeight: 280 }}>
-                     Content
-                  </Content>
-               </Layout>
-            </Content>
+            <Layout>
+               <Header className="header">
+                  <div id="hearder-titile">
+                     数据分析系统
+               </div>
+                  <div id="hearder-user">
+                     <Avatar icon="user" />
+                     <Dropdown overlay={menu}>
+                        <a id="hearder-user-dropdown-content" className="ant-dropdown-link" href="#">
+                           游客 <Icon type="down" />
+                        </a>
+                     </Dropdown>
+                  </div>
+               </Header>
+            </Layout>
          </Layout>
       );
    }
